d2d2acb93c3955d15b970ad55d893c0525710e27
LimaSystem <wolegario49@gmail.com>
2023-08-11 16:08:15 -0300

    Cria arquivo de configuração dos termos de uso e politica de privacidade

---
+++ compose/common/config.d/lgpd.php
@@ -0,0 +1,9 @@
+<?php 
+return [
+    'module.LGPD' => [
+        'termos-de-uso'=>[
+            'title'=> 'POLÍTICA DE PRIVACIDADE, TERMOS DE USO E AUTORIZAÇÃO DE USO DE IMAGEM', 
+            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/terms-of-usage.html')
+        ],
+    ]
+];
