229a2507bdef5b49804efb85afe7ca77a1001f89
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-08-16 18:39:30 -0300

    define a configuração do botão de aceite de termos de uso

--- compose/common/config.d/lgpd.php
+++ compose/common/config.d/lgpd.php
@@ -3,7 +3,8 @@ return [
     'module.LGPD' => [
         'termos-de-uso'=>[
             'title'=> 'POLÍTICA DE PRIVACIDADE, TERMOS DE USO E AUTORIZAÇÃO DE USO DE IMAGEM', 
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/terms-of-usage.html')
+            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/terms-of-usage.html'),
+            'buttonText' => 'Aceito os termos acima'
         ],
     ]
 ];
