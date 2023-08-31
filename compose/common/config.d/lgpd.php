2ea597d09566b441186a401ba4eaeb585d1c3ac0
wolegario40@gmail.com <wolegario49@gmail.com>
2023-08-31 10:36:32 -0300

    Corrigi os termos de uso

--- compose/common/config.d/lgpd.php
+++ compose/common/config.d/lgpd.php
@@ -2,7 +2,7 @@
 return [
     'module.LGPD' => [
         'termos-de-uso'=>[
-            'title'=> 'POLÍTICA DE PRIVACIDADE, TERMOS DE USO E AUTORIZAÇÃO DE USO DE IMAGEM', 
+            'title'=> 'Política de Privacidade, Termos de Uso e Autorização de Uso de Imagem', 
             'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/terms-of-usage.html'),
             'buttonText' => 'Aceito os termos acima'
         ],
