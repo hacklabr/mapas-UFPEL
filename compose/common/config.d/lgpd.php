d5dae8a86b398f2a0c77e276c21c033784dbf597
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-08-31 21:31:05 -0300

    atualização dos termos de uso

--- compose/common/config.d/lgpd.php
+++ compose/common/config.d/lgpd.php
@@ -1,10 +1,16 @@
 <?php 
+
+$routes = $config['routes'];
+$routes['shortcuts']['termos-de-uso'] = ['lgpd','view', ['termos-de-uso']];
+
 return [
     'module.LGPD' => [
         'termos-de-uso'=>[
             'title'=> 'Política de Privacidade, Termos de Uso e Autorização de Uso de Imagem', 
             'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/terms-of-usage.html'),
-            'buttonText' => 'Aceito os termos acima'
+            'buttonText' => 'Declaro que autorizo o uso de imagem e concordo com a Política de Privacidade, Termos de Uso e Autorização de Uso de Imagem da Plataforma Mapa Cultural AGIMOS'
         ],
-    ]
+    ],
+
+    'routes' => $routes
 ];
