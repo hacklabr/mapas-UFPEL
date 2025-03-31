9e0536a9d438bef52d870be101ab0581a3087a26
erleibiazzio <erleibiazzio@gmail.com>
2025-03-31 12:59:21 -0300

    Ajusta data do arquivo offline.php

--- docker/common/config.d/offline.php
+++ docker/common/config.d/offline.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'app.offline' => date('Y-m-d H:i:s') > '2025-03-31 08:30:00' && date('Y-m-d H:i:s') < '2025-04-25 09:00:00',
+    'app.offline' => date('Y-m-d H:i:s') > '2025-03-01 08:30:00' && date('Y-m-d H:i:s') < '2025-03-01 09:00:00',
     'app.offlineUrl' => '/em-breve',
     'app.offlineBypassFunction' => function() {
         $senha = $_GET['online'] ?? '';
